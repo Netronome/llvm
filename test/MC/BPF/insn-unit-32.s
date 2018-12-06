607f088b92ebfb09f026a84a9443a59237cf6628
Jiong Wang <jiong.wang@netronome.com>
2018-12-06 14:32:51 -0500

    bpf: add code-gen support for JMP32 instructions
    JMP32 instructions has been added to eBPF ISA. They are 32-bit variants of
    existing BPF conditional jump instructions, but the comparison happens on
    low 32-bit sub-register only, therefore some unnecessary extensions could
    be saved.
    
    JMP32 instructions will only be available for -mcpu=v3. Host probe hook has
    been updated accordingly.
    
    JMP32 instructions will only be enabled in code-gen when -mattr=+alu32
    enabled, meaning compiling the program using sub-register mode.
    
    For JMP32 encoding, it has the same BPF_JMPclass, but when
    BPF_SRC(code) == BPF_X, insn->imm contains sub-opcode and 0x1 means do
    comparison on 32-bit sub-register. When BPF_SRC(code) == BPF_K, the
    sub-opcode is at insn->src_reg.
    
    This patch has been tested by compiling and running kernel bpf selftests
    with JMP32 enabled.
    
    Signed-off-by: Jiong Wang <jiong.wang@netronome.com>

--- test/MC/BPF/insn-unit-32.s
+++ test/MC/BPF/insn-unit-32.s
@@ -54,3 +54,50 @@
 // CHECK: b4 09 00 00 01 00 00 00      w9 = 1
 // CHECK: b4 09 00 00 ff ff ff ff      w9 = -1
 // CHECK: c4 0a 00 00 40 00 00 00      w10 s>>= 64
+
+  if w0 == w1 goto Llabel0   // BPF_JEQ  | BPF_X
+  if w3 != w4 goto Llabel0   // BPF_JNE  | BPF_X
+// CHECK: 1d 10 0b 00 01 00 00 00 	if w0 == w1 goto +11
+// CHECK: 5d 43 0a 00 01 00 00 00 	if w3 != w4 goto +10
+
+  if w1 > w2 goto Llabel0    // BPF_JGT  | BPF_X
+  if w2 >= w3 goto Llabel0   // BPF_JGE  | BPF_X
+  if w4 s> w5 goto Llabel0   // BPF_JSGT | BPF_X
+  if w5 s>= w6 goto Llabel0  // BPF_JSGE | BPF_X
+// CHECK: 2d 21 09 00 01 00 00 00 	if w1 > w2 goto +9
+// CHECK: 3d 32 08 00 01 00 00 00 	if w2 >= w3 goto +8
+// CHECK: 6d 54 07 00 01 00 00 00 	if w4 s> w5 goto +7
+// CHECK: 7d 65 06 00 01 00 00 00 	if w5 s>= w6 goto +6
+
+  if w6 < w7 goto Llabel0    // BPF_JLT  | BPF_X
+  if w7 <= w8 goto Llabel0   // BPF_JLE  | BPF_X
+  if w8 s< w9 goto Llabel0   // BPF_JSLT | BPF_X
+  if w9 s<= w10 goto Llabel0 // BPF_JSLE | BPF_X
+// CHECK: ad 76 05 00 01 00 00 00 	if w6 < w7 goto +5
+// CHECK: bd 87 04 00 01 00 00 00 	if w7 <= w8 goto +4
+// CHECK: cd 98 03 00 01 00 00 00 	if w8 s< w9 goto +3
+// CHECK: dd a9 02 00 01 00 00 00 	if w9 s<= w10 goto +2
+
+  if w0 == 0 goto Llabel0           // BPF_JEQ  | BPF_K
+  if w3 != -1 goto Llabel0          // BPF_JNE  | BPF_K
+// CHECK: 15 10 01 00 00 00 00 00 	if w0 == 0 goto +1
+// CHECK: 55 13 00 00 ff ff ff ff 	if w3 != -1 goto +0
+
+Llabel0 :
+  if w1 > 64 goto Llabel0           // BPF_JGT  | BPF_K
+  if w2 >= 0xffffffff goto Llabel0  // BPF_JGE  | BPF_K
+  if w4 s> 0xffffffff goto Llabel0  // BPF_JSGT | BPF_K
+  if w5 s>= 0x7fffffff goto Llabel0 // BPF_JSGE | BPF_K
+// CHECK: 25 11 ff ff 40 00 00 00 	if w1 > 64 goto -1
+// CHECK: 35 12 fe ff ff ff ff ff 	if w2 >= -1 goto -2
+// CHECK: 65 14 fd ff ff ff ff ff 	if w4 s> -1 goto -3
+// CHECK: 75 15 fc ff ff ff ff 7f 	if w5 s>= 2147483647 goto -4
+
+  if w6 < 0xff goto Llabel0         // BPF_JLT  | BPF_K
+  if w7 <= 0xffff goto Llabel0      // BPF_JLE  | BPF_K
+  if w8 s< 0 goto Llabel0           // BPF_JSLT | BPF_K
+  if w9 s<= -1 goto Llabel0         // BPF_JSLE | BPF_K
+// CHECK: a5 16 fb ff ff 00 00 00 	if w6 < 255 goto -5
+// CHECK: b5 17 fa ff ff ff 00 00 	if w7 <= 65535 goto -6
+// CHECK: c5 18 f9 ff 00 00 00 00 	if w8 s< 0 goto -7
+// CHECK: d5 19 f8 ff ff ff ff ff 	if w9 s<= -1 goto -8
